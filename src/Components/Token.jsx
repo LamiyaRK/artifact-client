b4b560f125c46ba352d2cea30c47bcb8a3801d88
Lamiya <akabane755@gmail.com>
2025-06-17 23:47:12 +0600

    added delete

--- src/Components/Token.jsx
+++ src/Components/Token.jsx
@@ -3,7 +3,7 @@ import AuthContext from '../Context/AuthContext';
 
 const Token = () => {
     const {user}=use(AuthContext)
-    return user.accessToke;
+    return user.accessToken;
 };
 
 export default Token;
